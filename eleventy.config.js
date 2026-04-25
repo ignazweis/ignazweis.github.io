e2dbeb68269e5a04cebea501b7e3f72a0208f213
ignazweis <213210679+ignazweis@users.noreply.github.com>
2026-04-25 16:02:10 -0300

    fix: change code for date of posts to show correct date

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,6 @@
 export default function (eleventyConfig) {
-
     eleventyConfig.addFilter("formatDate", (value) => {
-        return new Intl.DateTimeFormat("pt-BR", {
-            timeZone: "America/Sao_Paulo",
-            day: "2-digit",
-            month: "2-digit",
-            year: "numeric"
-        }).format(value);
+        const [year, month, day] = value.toISOString().split("T")[0].split("-");
+        return `${day}/${month}/${year}`;
     });
-
 }
