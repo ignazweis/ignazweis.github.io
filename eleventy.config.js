e57de14c6c78168e52fbb9e4e97a7c3933f183bb
ignazweis <213210679+ignazweis@users.noreply.github.com>
2026-04-27 15:50:46 -0300

    feat: add generic fonts sans-serif to style body of html

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 export default function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addFilter("formatDate", (value) => {
         const [year, month, day] = value.toISOString().split("T")[0].split("-");
         return `${day}/${month}/${year}`;
